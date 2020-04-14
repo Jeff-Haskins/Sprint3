58e4f544eeb363023c6b30f50d81ca9ac427abe6
Jeff Haskins <jhaskins20@gmail.com>
2020-04-14 14:32:26 -0700

    Wishlist page more or less finished.

--- Sprint3_MyStore_Project/WebContent/Javascript/wishlist.js
+++ Sprint3_MyStore_Project/WebContent/Javascript/wishlist.js
@@ -7,13 +7,14 @@ $(document).ready(function() {
 		wishlistDiv.html(`<br><h4 style="text-align: center">Your wishlist is empty.</h4><br>`);
 	} else {
 		var wishlistTable = 
-			`<table class="table">
+			`<br><h4 style="text-align:center">Your Wishlist</h4><br>
+			<table class="table">
 				<thead>
 					<tr>
 						<th></th>
 						<th>Product</th>
-						<th>Qty</th>
 						<th>Price</th>
+						<th>Options</th>
 					</tr>
 				</thead>
 				<tbody>`;
@@ -24,9 +25,10 @@ $(document).ready(function() {
 					`<tr>
 						<td><img src="${product.img}" class="img-cart" width="auto" height="100"></td>
 						<td style="max-width:200px"><strong>${product.name}</strong></td>
+						<td id="price">$${product.price}</td>
 						<td>
 						<form class="form-inline">
-							<button class="btn btn-primary btnEdit"
+							<button class="btn btn-primary btnCart"
 								style="background-color: #45c04f; border: none; 
 								padding-left: 10px; padding-right: 10px;
 								margin-right: 10px">
@@ -39,7 +41,6 @@ $(document).ready(function() {
 								</button>
 						</form>
 						</td>
-						<td>$${product.price}</td>
 					</tr>`);
 		});
 		
@@ -55,17 +56,54 @@ $(document).ready(function() {
 		location.reload();
 	});
 	
+	$(".btnCart").click(function(e) {
+		e.preventDefault();
+		var div = $(this).parentsUntil("tr").parent();
+		var name = div.find("strong").html();
+		var price = div.find("#price").html().replace('$', '');
+		var qty = 1;
+		var img = div.find("img").attr("src");
+		
+		var cart = JSON.parse(sessionStorage.getItem("cart"));
+		var exists = false;
+		
+        if (cart == null) {
+            cart = [];
+        } else {
+        	cart.forEach(product => {
+        		if (product.name == name) {
+        			exists = true;
+        			product.qty += qty;
+        			sessionStorage.setItem("cart", JSON.stringify(cart));
+        		}
+        	});
+        }
+        
+        if (!exists) {
+    		var item = {
+    			name:name,
+    			price:price,
+    			qty:qty,
+    			img:img
+    		};
+    		cart.push(item);
+    		sessionStorage.setItem("cart", JSON.stringify(cart));
+    	}
+        
+        window.location.href = "myCart.html";
+	});
+	
 	$("#test").click(function() {
 		wishlist = [];
 		var testItem = {
 				name: "Test Product",
 				price: "9.95",
-				img: null
+				img: "../Image/catcostume.jpg"
 		};
 		var testItem2 = {
 				name: "Test Product",
 				price: "9.95",
-				img: null
+				img: "../Image/catcostume.jpg"
 		};
 		wishlist.push(testItem);
 		wishlist.push(testItem2);
