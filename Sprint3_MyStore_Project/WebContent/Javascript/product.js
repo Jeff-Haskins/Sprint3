4a60edcb335a9761c40ce668810c104e442e1bd5
Monica Parente <mon8806@gmail.com>
2020-04-15 09:08:34 -0700

    added filter options

--- Sprint3_MyStore_Project/WebContent/Javascript/product.js
+++ Sprint3_MyStore_Project/WebContent/Javascript/product.js
@@ -1,21 +1,92 @@
 $(document).ready(function(){
 	//store user input to local storage
-	$("#searchButton").on("click", function(e){
+	$("#searchButton").on("click", function(){
 		localStorage.setItem("searchInput", $("#searchProductInput").val());
 		searchProduct();
 		//clears out the search
 		localStorage.setItem("searchInput", "");
 		//refresh page on search button
 		$("#searchButton").click(function(){
-			location.reload();
-			
+			location.reload();		
 		})
 	});
 	
+	$("#lessThanFive").on("click", function(){
+		resetPage()
+		var cardPrice = $(".price")
+		console.log(cardPrice)
+		for(var i=0; i<cardPrice.length; i++){
+			var parsedPrice = parseFloat(cardPrice[i].innerText.slice(1,6));
+			console.log(parsedPrice)
+			if(parsedPrice > 5) {
+				var cards=$(cardPrice[i]).parentsUntil(".col");
+				console.log(cards)
+				$(cards[2]).hide();
+			}	
+		}
+	})
+	
+	$("#tenToTwenty").on("click", function(){
+		resetPage()
+		var cardPrice = $(".price")
+		console.log(cardPrice)
+		for(var i=0; i<cardPrice.length; i++){
+			var parsedPrice = parseFloat(cardPrice[i].innerText.slice(1,6));
+			console.log(parsedPrice)
+			if(parsedPrice >20 || parsedPrice <10) {
+					var cards=$(cardPrice[i]).parentsUntil(".col");
+					console.log(cards)
+					$(cards[2]).hide();		
+			}	
+		}
+	})
+	
+	$("#twentyToFifty").on("click", function(){
+		resetPage()
+		var cardPrice = $(".price")
+		console.log(cardPrice)
+		for(var i=0; i<cardPrice.length; i++){
+			var parsedPrice = parseFloat(cardPrice[i].innerText.slice(1,6));
+			console.log(parsedPrice)
+			if(parsedPrice >50 || parsedPrice <20) {
+					var cards=$(cardPrice[i]).parentsUntil(".col");
+					console.log(cards)
+					$(cards[2]).hide();		
+			}	
+		}
+	})
+	
+	$("#fiftyToOneHundred").on("click", function(){
+		resetPage()
+		var cardPrice = $(".price")
+		console.log(cardPrice)
+		for(var i=0; i<cardPrice.length; i++){
+			var parsedPrice = parseFloat(cardPrice[i].innerText.slice(1,6));
+			console.log(parsedPrice)
+			if(parsedPrice >100 || parsedPrice <50) {
+					var cards=$(cardPrice[i]).parentsUntil(".col");
+					console.log(cards)
+					$(cards[2]).hide();		
+			}	
+		}
+	})
+	
+	function resetPage(){
+		var cardPrice = $(".price")
+		console.log(cardPrice)
+		for(var i=0; i<cardPrice.length; i++){
+			var parsedPrice = parseFloat(cardPrice[i].innerText);
+			console.log(parsedPrice)
+				var cards=$(cardPrice[i]).parentsUntil(".col");
+				console.log(cards)
+				$(cards[2]).show();
+	
+		}
+	}
 	//Called on page load
 	searchProduct();
 	//checks to see if user input in search matches anything in local storage
-	function searchProduct(){
+	function searchProduct(e){
 		var getInput = localStorage.getItem("searchInput");
 		//if user input isnt null or empty it will iterate through the cards div array
 		if(getInput != null || getInput != ""){
@@ -30,4 +101,6 @@ $(document).ready(function(){
 			}
 		}e.preventDefault();
 	}
+	
+	
 });
